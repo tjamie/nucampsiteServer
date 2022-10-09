44f865a34b8da71a16cd2c062fd13046d40ca4c2
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2022-10-09 18:35:36 -0400

    user authentication with passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -47,26 +49,22 @@ app.use(
         store: new FileStore()
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 //authentication
 function auth(req, res, next) {
-    console.log(req.session);
-    if (!req.session.user) {
+    console.log(req.user);
+    if (!req.user) {
         // console.log(req.headers);
         const err = new Error("You are not authenticated");
         err.status = 401;
         return next(err);
     } else {
-        if (req.session.user === "authenticated") {
-            return next();
-        } else {
-            const err = new Error("You are not authorized");
-            err.status = 401;
-            return next(err);
-        }
+        return next();
     }
 }
 app.use(auth);
