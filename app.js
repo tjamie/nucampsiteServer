7dd2ae8d90b0c242adbf687b8c1cc5c1d3f9300c
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2022-10-22 14:21:52 -0400

    workshop 4

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
 
 var app = express();
 
@@ -58,6 +59,7 @@ app.use("/campsites", campsiteRouter);
 app.use("/partners", partnerRouter);
 app.use("/promotions", promotionRouter);
 app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
